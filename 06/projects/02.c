afdfd54db2881d359a4386f001af9c6355ab2f4a
tokisuno <lucaswmei@gmail.com>
2024-11-06 17:45:17 -0500

    chapter 6 things

---
+++ 06/projects/02.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int m, n;
+    int gcd;
+    int remainder;
+
+    printf("Enter two integers separated by a space: ");
+    scanf("%d %d", &m, &n);
+
+    // *** Use Euclid's algorithm ***
+
+    return 0;
+}
